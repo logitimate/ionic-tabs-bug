7457e8fbfd749cca00aaa769de62a48adaa0103a
pairing <pairing@mc07z916ejyw0.dds.dillards.net>
2020-02-25 11:29:13 -0600

    adjusting bug example

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+    selector: 'app-tab1',
+    templateUrl: 'tab1.page.html',
+    styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+    public content: string[] = new Array(50)
+        .fill(null)
+        .map(_ => 'This is an item');
 
-  constructor() {}
+    constructor() {
+    }
 
 }
